fd19ac1705107e43509680034a5957afb65a67e4
Misbahur <misbahur.rifqi61@gmail.com>
2024-05-16 11:38:27 +0700

    Seeder done isine

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class SuperAdminSeeder extends Seeder
 {
@@ -12,6 +14,28 @@ class SuperAdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Creating Super Admin User
+        $superAdmin = User::create([
+            'name' => 'Misbahur Rifqi', 
+            'email' => 'misbahur@gmail.com',
+            'password' => Hash::make('rifqi123')
+        ]);
+        $superAdmin->assignRole('Super Admin');
+
+        // Creating Admin User
+        $admin = User::create([
+            'name' => 'Rifqi Misbahur', 
+            'email' => 'rifqi@gmail.com',
+            'password' => Hash::make('rifqi123')
+        ]);
+        $admin->assignRole('Admin');
+
+        // Creating Product Manager User
+        $productManager = User::create([
+            'name' => 'Admin Kita', 
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123')
+        ]);
+        $productManager->assignRole('Product Manager');
     }
 }
